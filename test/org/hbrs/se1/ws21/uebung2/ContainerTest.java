37063125806e94467543c553605681f7010e8c29
Rudolph Kouakeu <rudolph.kouakeu@smail.inf.h-brs.de>
2021-10-17 22:29:56 +0200

    Test Klasse

---
+++ test/org/hbrs/se1/ws21/uebung2/ContainerTest.java
@@ -0,0 +1,32 @@
+package org.hbrs.se1.ws21.uebung2;
+
+import org.hbrs.se1.ws21.uebung2.exception.ContainerException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    Container container = new Container();
+        Member m1 = new MemberItem(11);
+        Member m2 = new MemberItem(22);
+        Member m3 = new MemberItem(33);
+        Member m4 = new MemberItem(44);
+
+    @Test
+    void containerTest() throws ContainerException {
+        assertEquals(0, container.size());
+
+        container.addMember(m1);
+        container.addMember(m2);
+        container.addMember(m3);
+        container.addMember(m4);
+        assertEquals(4, container.size());
+
+        assertEquals("ID nicht vorhanden", container.deleteMember(77));
+        assertEquals("Member mit ID 22 würde gelöscht", container.deleteMember(22));
+        assertEquals("Member mit ID 33 würde gelöscht", container.deleteMember(33));
+        assertEquals(2, container.size());
+        assertThrows(ContainerException.class, ()->{container.addMember(new MemberItem(44));});
+    }
+}
