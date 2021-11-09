a408e9615d99a236e96c5cdd8531ca91d1d2d6ca
Rudolph Kouakeu <rudolph.kouakeu@smail.inf.h-brs.de>
2021-11-10 00:19:51 +0100

    Employee class

--- src/org/hbrs/se1/ws21/uebung4/prototype/Employee.java
+++ src/org/hbrs/se1/ws21/uebung4/prototype/Employee.java
@@ -1,14 +1,45 @@
 package org.hbrs.se1.ws21.uebung4.prototype;
 
-public class Employee implements java.io.Serializable, Comparable {
+public class Employee implements java.io.Serializable, Comparable<Employee> {
 
     private String vorname;
     private String name;
     private Integer pid;
     private String abteilung;
+    private String expertise;
+    private String rolle;
+
+    public Employee(Integer pid, String vorname, String nachname, String rolle, String abteilung, String expertise) {
+        this.pid = pid;
+        this. name = nachname;
+        this.vorname = vorname;
+        this.abteilung = abteilung;
+        this.expertise = expertise;
+        this.rolle = rolle;
+    }
+
+    public Employee() {
+
+    }
 
     public String getAbteilung() {
         return abteilung;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public String getRolle() {
+        return this.rolle;
+    }
+
+    public void setRolle(String rolle) {
+        this.rolle = rolle;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
     }
 
     public void setAbteilung(String abteilung) {
@@ -40,8 +71,9 @@ public class Employee implements java.io.Serializable, Comparable {
     }
 
     @Override
-    public int compareTo(Object o) {
-        return 0; // Werte: 0, 1, -1 --> Pid vergleichen!
+    public int compareTo(Employee o) {
+        // Werte: 0, 1, -1 --> Pid vergleichen!
+        return this.pid.compareTo(o.getPid());
     }
 }
 
